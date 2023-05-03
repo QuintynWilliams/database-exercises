05c1014c47a813113c58c328460736767f2a2879
Quintyn Williams <t.quin.williams@gmail.com>
2023-05-03 15:50:37 -0500

    mySQL group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -22,3 +22,34 @@ SELECT MAX(DATEDIFF(hire_date, birth_date)) / 365 FROM employees;
 
 # Average age of all employees
 SELECT MAX(DATEDIFF(hire_date, birth_date)) / 365 FROM employees;
+
+
+# TODO: In your script, use DISTINCT to find the unique titles in the titles table.
+SELECT DISTINCT title FROM titles;
+
+# TODO: Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
+SELECT DISTINCT last_name FROM employees
+                          WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+                          GROUP BY last_name;
+
+# TODO: Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT DISTINCT (CONCAT(first_name, ' ', last_name)) FROM employees
+                          WHERE (last_name LIKE 'E%' AND last_name LIKE '%E')
+
+# TODO: Find the unique last names with a 'q' but not 'qu'.
+SELECT DISTINCT last_name FROM employees
+                          WHERE last_name LIKE '%q%'
+                            AND last_name NOT LIKE '%qu%';
+
+# TODO: Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT last_name, COUNT(first_name) FROM employees
+                          WHERE last_name LIKE '%q%'
+                            AND last_name NOT LIKE '%qu%'
+                          GROUP BY last_name;
+
+# TODO: Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT gender, COUNT(gender) FROM employees
+                             WHERE first_name = 'Irena'
+                                OR first_name = 'Vidya'
+                                OR first_name = 'Maya'
+                             GROUP BY gender;
