37f345cfe28f3151c3ec14f8fb33db9b8e969eea
Quintyn Williams <t.quin.williams@gmail.com>
2023-05-05 08:34:00 -0500

    initialize

---
+++ join_exercises.sql
@@ -0,0 +1,41 @@
+USE employees;
+
+SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+
+
+# TODO: Show each department along with the name of the current manager for that department.
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager' FROM dept_manager mgr
+    JOIN dept_emp de
+        ON de.emp_no = mgr.emp_no
+    JOIN departments d
+        ON d.dept_no = de.dept_no
+    JOIN employees e
+        ON mgr.emp_no = e.emp_no
+    WHERE mgr.to_date > NOW()
+    ORDER BY dept_name ASC;
+
+# TODO: Find the name of all departments currently managed by women.
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager' FROM dept_manager mgr
+    JOIN dept_emp de
+        ON de.emp_no = mgr.emp_no
+    JOIN departments d
+        ON d.dept_no = de.dept_no
+    JOIN employees e
+        ON mgr.emp_no = e.emp_no
+    WHERE mgr.to_date > NOW()
+      AND e.gender = 'F'
+    ORDER BY dept_name ASC;
+
+# TODO: Find the current titles of employees currently working in the Customer Service department.
+
+
+
+# TODO: Find the current salary of all current managers.
+
+
+
+# TODO: [BONUS] Find the names of all current employees, their department name, and their current manager's name .
+
+
